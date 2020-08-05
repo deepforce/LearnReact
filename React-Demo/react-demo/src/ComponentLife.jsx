0fe19b9f92d8b06a549ea903a0a8cd0474a8dcd3
Zack <deepforce31@gmail.com>
2020-08-04 18:34:28 -0700

    React Lifecycle

---
+++ React-Demo/react-demo/src/ComponentLife.jsx
@@ -0,0 +1,61 @@
+import React from "react"
+
+export default class ComponentLife extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 10
+        }
+    }
+
+    componentWillMount() {
+        console.log("componentWillMount");
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate");
+        return true;
+    }
+
+    componentWillUpdate() {
+        console.log("componentWillUpdate");
+    }
+
+    componentDidUpdate() {
+        console.log("componentDidUpdate");
+    }
+
+    componentWillReceiveProps() {
+        console.log("componentWillReceiveProps");
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
+    changeHandler = () => {
+        this.setState({
+            count: this.state.count+=1
+        })
+    }
+
+    clickChange = () => {
+        this.props.clickChanges("我是儿子的数据");
+    }
+
+    render() {
+        const { count } = this.state
+        return(
+            <div>
+                生命周期函数: { count } - { this.props.title }
+                <button onClick={ this.changeHandler }>修改</button>
+                <button onClick={ this.clickChange }>修改title</button>
+            </div>
+        )
+    }
+}
--- React-Demo/react-demo/src/app.jsx
+++ React-Demo/react-demo/src/app.jsx
@@ -1,9 +1,24 @@
 import React from "react"
-import Home from "./Home"
-import MyNav from "./MyNav"
-import StateComponent from "./StateComponent"
+// import Home from "./Home"
+// import MyNav from "./MyNav"
+// import StateComponent from "./StateComponent"
+import ComponentLife from "./ComponentLife"
 // 用类的形式创建组件，Hook形式
 class App extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {
+            title:"文本1"
+        }
+    }
+
+    clickChange = (data) => {
+        this.setState({
+            title: data
+        });
+    }
+
     // 渲染函数
     render() {
 
@@ -17,7 +32,9 @@ class App extends React.Component{
                 <Home />
                 <MyNav nav={ nav1 } title = "路径导航"/>
                 <MyNav nav={ nav2 } title = "学习导航"/> */}
-                <StateComponent />
+                {/* <StateComponent /> */}
+                <ComponentLife title = { this.state.title } clickChanges = { this.clickChange }/>
+                {/* <button onClick={ this.clickChange }>修改title</button> */}
             </div>
         )
     }
